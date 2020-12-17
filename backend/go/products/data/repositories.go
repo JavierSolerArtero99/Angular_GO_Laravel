c515cf4f02f66142c23a22deee82d3a82268c9fc
Javier Soler Artero <javier.soler.jsa@gmail.com>
2020-12-17 21:31:13 +0100

    doing relationships

--- backend/go/products/data/repositories.go
+++ backend/go/products/data/repositories.go
@@ -11,4 +11,12 @@ func FindProducts() ([]models.Products, error) {
 	var model []models.Products
 	err := db.Find(&model).Error
 	return model, err
+	// db := common.GetDB()
+	// var model []models.Products
+	// tx := db.Begin()
+	// tx.Find(&model)
+	// tx.Model(&model).Related(&model.Author, "Author")
+	// tx.Model(&model.Author).Related(&model.Author)
+	// err := tx.Commit().Error
+	// return model, err
 }
--- backend/go/products/models/models.go
+++ backend/go/products/models/models.go
@@ -7,5 +7,20 @@ type (
 	Products struct {
 		// Id        bson.ObjectId `bson:"_id,omitempty" json:"id"`
 		Name      string        `json:"name"`
+		Author	  int			`json: user`
+	}
+)
+
+type (
+	Author struct {
+		ID           uint    `gorm:"primary_key"`
+		Username     string  `gorm:"column:username"`
+		Email        string  `gorm:"column:email;unique_index"`
+		Bio          string  `gorm:"column:bio;size:1024"`
+		Image        *string `gorm:"column:image"`
+		PasswordHash string  `gorm:"column:password;not null"`
+		Role         bool    `gorm:"column:role;default 0 not null"`
+		Karma        uint    `gorm:"column:karma;default 0 not null"`
+		Tempkey      string  `gorm:"column:tempkey`
 	}
 )
