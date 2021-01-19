72fa876179fd44efebd198d485fe678e3ce29232
Javier Soler Artero <javier.soler.jsa@gmail.com>
2021-01-19 11:29:12 +0100

    blank stat

--- frontend/src/app/panel-admin/panel/panel.component.ts
+++ frontend/src/app/panel-admin/panel/panel.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-panel',
-  templateUrl: './panel.component.html',
-  styles: []
+  selector: "app-panel",
+  templateUrl: "./panel.component.html",
+  styles: [],
 })
 export class PanelComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  isNewProduct: boolean = false;
 
-  
+  ngOnInit() {}
 
-  ngOnInit() {
+  toogleNewProductForm():void {
+    this.isNewProduct = !this.isNewProduct;  
   }
-
 }
